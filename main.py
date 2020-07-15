ee66d82a83b49b498491e840a283cfd55dc1a32c
Lightlyflow <adensun887@gmail.com>
2020-07-15 14:17:52 -0500

    Added apple move and overlapping detection.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import tkinter as tk
+from random import random, randrange
+
 from PIL import ImageTk, Image
 
 direction = 'up'
 player_bbox = (240, 240, 260, 260)
 UPDATE_TIME = 100
-apple_coords = (0, 0)
-apple_img = None
+apple_coords = (110, 110)
+score = 0
+
 
 class Board(tk.Canvas):
-    global direction
+    global direction, score
 
     def __init__(self, master):
         super().__init__(bg='black', master=master)
@@ -19,7 +22,7 @@ class Board(tk.Canvas):
         self.create_entities()
 
         self.bind_all("<Key>", self.key_press)
-        self.after(500, self.move_player)
+        self.after(500, self.game_loop)
         pass
 
     def create_entities(self):
@@ -27,7 +30,7 @@ class Board(tk.Canvas):
         apple_img = ImageTk.PhotoImage(Image.open('res/apple.png'))
         self.image = apple_img
         self.apple = self.create_image(apple_coords, tags='apple', image=apple_img)
-        self.score = self.create_text(480, 5, anchor='ne', text='Score: 0', fill='white')
+        self.score_text = self.create_text(480, 5, anchor='ne', text='Score: 0', fill='white')
 
     def key_press(self, event):
         global direction
@@ -41,16 +44,7 @@ class Board(tk.Canvas):
         elif button_press == 'd':
             direction = 'right'
         elif button_press == 'x':
-            print('Height:', self.winfo_height())
-            print('Width:', self.winfo_width())
-
-    # Game loop
-    def move_player(self):
-        dx, dy = self.get_movement()
-
-        self.move(self.player, dx, dy)
-
-        self.after(UPDATE_TIME, self.move_player)
+            print('Apple loc:', self.coords(self.apple))
 
     def get_movement(self):
         temp_bbox = self.coords(self.player)
@@ -75,6 +69,36 @@ class Board(tk.Canvas):
             dy = 0
 
         return dx, dy
+
+    def isOverlapping(self) -> bool:
+        global player_bbox
+
+        player_bbox = self.coords(self.player)
+        if len(self.find_overlapping(*player_bbox)) > 1:
+            return True
+        return False
+
+    def move_apple(self):
+        # TODO:: Check for snake body
+        x, y = randrange(0, stop=500, step=20)+10, randrange(0, stop=500, step=20)+10
+
+        self.coords(self.apple, x, y)
+
+    # Game loop
+    def game_loop(self):
+        global score
+
+        # Movement
+        dx, dy = self.get_movement()
+        self.move(self.player, dx, dy)
+
+        # Collision with apple
+        if self.isOverlapping():
+            score += 1
+            self.itemconfigure(self.score_text, text='Score: '+str(score))
+            self.move_apple()
+
+        self.after(UPDATE_TIME, self.game_loop)
 
 
 class Snake(tk.Frame):
